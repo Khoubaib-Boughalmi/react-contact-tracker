be6d90fc43a8865c48ba13a23bf5d13e34c4c3e1
Khoubaib Boughalmi <khoubaib.kb99@gmail.com>
2023-11-20 19:53:01 +0100

    added child elements to root contacts

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import Root from "./routes/root";
+import Root, { loader as rootLoader } from "./routes/root";
 import Contact from "./routes/contact";
 import ErrorPage from "./error-page";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: "/contacts/:contactId",
